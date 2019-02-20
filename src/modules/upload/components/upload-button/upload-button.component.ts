ca274cb2297935d2d77fe60a01f4a7ce61f9abc4
Daniel Ehrhardt <daniel.ehrhardt@iea-hamburg.de>
2019-02-20 11:52:25 +0100

    Fixed Upload Bug

--- src/modules/upload/components/upload-button/upload-button.component.ts
+++ src/modules/upload/components/upload-button/upload-button.component.ts
@@ -1,17 +1,7 @@
-import {
-  Component,
-  OnInit,
-  ViewChild
-} from '@angular/core';
-import {
-  forkJoin
-} from 'rxjs';
-import {
-  UploadService
-} from '../../services/upload.service';
-import {
-  ToastController
-} from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { forkJoin } from 'rxjs';
+import { UploadService } from '../../services/upload.service';
 declare var kendo: any;
 
 @Component({
@@ -38,90 +28,90 @@ export class UploadButtonComponent implements OnInit {
   ngOnInit() {
 
     kendo.jQuery(this.imageUpload.nativeElement).kendoImageBrowser({
-        imageBrowser: {
-          messages: {
-            dropFilesHere: 'Drop files here'
-          },
-          transport: {
-            read: '../ImageBrowser/Read',
-            destroy: {
-              url: '../ImageBrowser/Destroy',
-              type: 'POST'
-            },
-            create: {
-              url: '../ImageBrowser/Create',
-              type: 'POST'
-            },
-            thumbnailUrl: '../ImageBrowser/Thumbnail',
-            uploadUrl: '../ImageBrowser/Upload',
-            imageUrl: '~/Content/UserFiles/Upload/'
-          },
-          change: (e) => {
-            // const imageUrl = this.value() + this.helperService.getCookie('X-IEA-Study') + '/' + e.selected.name;
-            console.log(e);
-
-          }
-        }
-      });
-
-    }
-
-    addFiles() {
-      this.file.nativeElement.click();
-    }
-
-    onFilesAdded() {
-      const files: {
-        [key: string]: File
-      } = this.file.nativeElement.files;
-      for (const key in files) {
-        if (!isNaN(parseInt(key))) {
-          this.files.add(files[key]);
-        }
-      }
-    }
-
-    upload() {
-      // if everything was uploaded already, just close the dialog
-      if (this.uploadSuccessful) {
+      messages: {
+        dropFilesHere: 'Drop files here'
+      },
+      transport: {
+        read: {
+          url: '../../ImageBrowser/Read'
+        },
+        destroy: {
+          url: '../../ImageBrowser/Destroy',
+          type: 'POST'
+        },
+        create: {
+          url: '../../ImageBrowser/Create',
+          type: 'POST'
+        },
+        thumbnailUrl: '../../ImageBrowser/Thumbnail',
+        uploadUrl: '../../ImageBrowser/Upload',
+        imageUrl: '../../Content/UserFiles/Upload/'
+      },
+      change: (e) => {
+        // const imageUrl = this.value() + this.helperService.getCookie('X-IEA-Study') + '/' + e.selected.name;
+        console.log(e);
 
       }
-      this.uploading = true;
-
-      this.progress = this.uploadService.upload(this.files);
-
-      const allProgressObservables = [];
-      for (const key in this.progress) {
-        allProgressObservables.push(this.progress[key].progress);
-      }
-
-      this.primaryButtonText = 'Finish';
-      this.canBeClosed = false;
-      this.showCancelButton = false;
-
-      // When all progress-observables are completed...
-      forkJoin(allProgressObservables).subscribe(async end => {
-
-        this.uploadSuccessful = true;
-        this.uploading = false;
-        this.progress = 0;
-        const toast = await this.toastCtrl.create({
-          message: 'Upload Successful',
-          duration: 3000
-        });
-        toast.present();
-      }, async (error) => {
-        this.progress = 0;
-        const toast = await this.toastCtrl.create({
-          message: 'Upload failed',
-          duration: 3000
-        });
-        toast.present();
-        console.log(error);
-        this.uploadSuccessful = false;
-        this.uploading = false;
-      });
-
-    }
+    });
 
   }
+
+  addFiles() {
+    this.file.nativeElement.click();
+  }
+
+  onFilesAdded() {
+    const files: {
+      [key: string]: File
+    } = this.file.nativeElement.files;
+    for (const key in files) {
+      if (!isNaN(parseInt(key))) {
+        this.files.add(files[key]);
+      }
+    }
+  }
+
+  upload() {
+    // if everything was uploaded already, just close the dialog
+    if (this.uploadSuccessful) {
+
+    }
+    this.uploading = true;
+
+    this.progress = this.uploadService.upload(this.files);
+
+    const allProgressObservables = [];
+    for (const key in this.progress) {
+      allProgressObservables.push(this.progress[key].progress);
+    }
+
+    this.primaryButtonText = 'Finish';
+    this.canBeClosed = false;
+    this.showCancelButton = false;
+
+    // When all progress-observables are completed...
+    forkJoin(allProgressObservables).subscribe(async end => {
+
+      this.uploadSuccessful = true;
+      this.uploading = false;
+      this.progress = 0;
+      const toast = await this.toastCtrl.create({
+        message: 'Upload Successful',
+        duration: 3000
+      });
+      toast.present();
+    }, async (error) => {
+      this.progress = 0;
+      const toast = await this.toastCtrl.create({
+        message: 'Upload failed',
+        duration: 3000
+      });
+      toast.present();
+      console.log(error);
+      this.uploadSuccessful = false;
+      this.uploading = false;
+    });
+
+  }
+
+}
