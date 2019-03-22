62a1ad9c5d6c3abacfcbf2edc8072e56f3c9594e
Daniel Ehrhardt <danielwespire@gmail.com>
2019-03-22 10:55:06 +0100

    Updated Build Logic for Angular Elements Build

--- angular-elements-build.js
+++ angular-elements-build.js
@@ -2,6 +2,8 @@ const fs = require('fs-extra');
 const concat = require('concat');
 var ncp = require('ncp').ncp;
 var rimraf = require("rimraf");
+
+
 (async function build() {
   const files = [
     './dist/runtime.js',
@@ -10,28 +12,33 @@ var rimraf = require("rimraf");
     './dist/main.js'
   ];
   // Delete Folder Elements
-  await rimraf('elements', async function () { 
-    return await console.log("Folder deleted");
+  rimraf('elements', async () => { 
+
+    console.log('1. Create Folder Elements');
+    // Create Folder Elements
+    await fs.ensureDir('elements');
+
+    console.log('2. Copy Files from dist to elements');
+    // Copy Files from dist to elements
+    await ncp('dist/', 'elements/');
+
+    console.log('3. Conact Files for Angular Elements');
+    // Conact Files for Angular Elements
+    await concat(files, 'elements/eassessment-content-editor.js');
+
+    console.log('4. CleanUp Old Files');
+    // CleanUp Old Files
+    for (let file of files) {
+      file = file.replace('dist', 'elements');
+      await fs.remove(file);
+    }
+
+    console.log('Rename Style File');
+    // Rename Style File
+    fs.rename('elements/styles.css', 'elements/eassessment-content-editor.css');
+
+    console.log('Copy Demo HTML File');
+    // Copy Demo HTML File
+    fs.copyFileSync('src/elements.html', 'elements/index.html');
   });
-  // Create Folder Elements
-  await fs.ensureDir('elements');
-
-  // Copy Files from dist to elements
-  await ncp('dist/', 'elements/');
-
-  // Conact Files for Angular Elements
-  await concat(files, 'elements/eassessment-content-editor.js');
-
-  // CleanUp Old Files
-  files.forEach(async (file) => {
-    file = file.replace('dist', 'elements');
-    await rimraf(file, async function () { 
-      await console.log("File deleted", file);
-    })
-  })
-  // Rename Style File
-  fs.rename('elements/styles.css', 'elements/eassessment-content-editor.css');
-
-  // Copy Demo HTML File
-  fs.copyFileSync('src/elements.html', 'elements/index.html');
 })();
