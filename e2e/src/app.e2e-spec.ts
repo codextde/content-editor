685457c3fff2917904f34992f8f96bd952039f98
Daniel Ehrhardt <daniel.ehrhardt@iea-hamburg.de>
2019-03-26 13:44:11 +0100

    Fixed e2e Tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,13 +1,5 @@
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('should go to the main page', () => {
-    page.navigateTo();
-  });
 });
